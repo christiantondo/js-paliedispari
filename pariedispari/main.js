d09031ca489e60071adcd72d36784d9071823058
christiantondo <christiantondo1@gmail.com>
2025-11-05 19:02:08 +0100

    prompt: l'utente sceglie pari/dispari e numero

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -1,2 +1,8 @@
 console.log("JS ok")
 
+let paridispariUtente = prompt("Scegli pari o dispari");
+console.log("L'utente ha scelto", paridispariUtente);
+
+let numeroUtente = prompt("Scegli un numero da 1 a 5");
+console.log("L'utente ha scelto", numeroUtente);
+
