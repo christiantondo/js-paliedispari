373371d3a7b7be4058fb88b8d709ce512f2054f3
christiantondo <christiantondo1@gmail.com>
2025-11-06 18:58:49 +0100

    palindroma senza funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,2 +1,17 @@
 console.log("JS ok")
 
+let input = prompt("Inserisci una parola");
+let inputReverse = "";
+
+// inputReverse = input.split("").reverse().join("");
+
+for (let i = input.length-1; i >= 0; i--) {
+    const character = input.charAt[i];
+    inputReverse += character;
+}
+
+if (input == inputReverse) {
+    alert("Hai trovato un palindromo");
+} else {
+    alert("Non è un palindromo");
+}
