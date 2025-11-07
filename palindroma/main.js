7f074829fad87106732882b72a854cdfe971805a
christiantondo <christiantondo1@gmail.com>
2025-11-07 22:05:11 +0100

    palindroma completo con funzione

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,17 +1,29 @@
 console.log("JS ok")
 
 let input = prompt("Inserisci una parola");
-let inputReverse = "";
+let inputReverse = reverseString(input);
 
 // inputReverse = input.split("").reverse().join("");
 
-for (let i = input.length-1; i >= 0; i--) {
+/*
+for (let i = input.length - 1; i >= 0; i--) {
     const character = input.charAt[i];
     inputReverse += character;
-}
+} */
 
 if (input == inputReverse) {
     alert("Hai trovato un palindromo");
 } else {
     alert("Non è un palindromo");
+}
+
+function reverseString(stringa) {
+    let stringaReversed = "";
+
+    for (let i = stringa.length - 1; i >= 0; i--) {
+        const character = stringa.charAt(i);
+        stringaReversed += character;
+    }
+
+    return stringaReversed
 }
